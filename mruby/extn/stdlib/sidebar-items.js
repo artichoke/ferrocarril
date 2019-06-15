a5b0c19f64c77524f865815bcf9db70c57aa03e1
ci-build <ci-build@hyperbo.la>
2019-06-15 00:59:48 +0000

    [skip ci] generate docs

--- mruby/extn/stdlib/sidebar-items.js
+++ mruby/extn/stdlib/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["patch",""]],"mod":[["delegate",""],["forwardable",""],["monitor",""],["ostruct",""],["set",""]]});
+initSidebarItems({"fn":[["patch",""]],"mod":[["delegate",""],["forwardable",""],["monitor",""],["ostruct",""],["set",""],["strscan",""]]});
