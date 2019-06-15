5d721615ea06ae03502d1446c0aba7897e31eac1
ci-build <ci-build@hyperbo.la>
2019-06-15 18:31:35 +0000

    [skip ci] generate docs

--- mruby/extn/core/sidebar-items.js
+++ mruby/extn/core/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["patch",""]],"mod":[["error",""],["kernel",""],["module",""],["regexp",""],["string",""],["thread",""]]});
+initSidebarItems({"fn":[["patch",""]],"mod":[["env",""],["error",""],["kernel",""],["module",""],["regexp",""],["string",""],["thread",""]]});
