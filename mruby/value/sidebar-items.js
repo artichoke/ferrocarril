0baf20efa327dca24023e58f02527361441425ce
ci-build <ci-build@hyperbo.la>
2019-06-25 22:32:11 +0000

    [skip ci] generate docs

--- mruby/value/sidebar-items.js
+++ mruby/value/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["MRB_FUNCALL_ARGC_MAX","Max argument count for function calls including initialize."]],"mod":[["types",""]],"struct":[["Value",""]],"trait":[["ValueLike",""]]});
+initSidebarItems({"constant":[["MRB_FUNCALL_ARGC_MAX","Max argument count for function calls including initialize."]],"mod":[["types",""]],"struct":[["Value","Wrapper around a [`sys::mrb_value`]."]],"trait":[["ValueLike",""]]});
