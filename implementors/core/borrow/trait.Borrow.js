0fd5bc6bc4f26fdd217bd3c4b40ec739e7d0ee89
ci-build <ci-build@hyperbo.la>
2019-07-13 08:06:55 +0000

    [skip ci] generate docs

--- implementors/core/borrow/trait.Borrow.js
+++ implementors/core/borrow/trait.Borrow.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["cactusref"] = [{text:"impl&lt;T:&nbsp;?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> + <a class=\"trait\" href=\"cactusref/trait.Reachable.html\" title=\"trait cactusref::Reachable\">Reachable</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/borrow/trait.Borrow.html\" title=\"trait core::borrow::Borrow\">Borrow</a>&lt;T&gt; for <a class=\"struct\" href=\"cactusref/struct.Rc.html\" title=\"struct cactusref::Rc\">Rc</a>&lt;T&gt;",synthetic:false,types:["cactusref::rc::Rc"]},];
+implementors["cactusref"] = [{text:"impl&lt;T:&nbsp;?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/borrow/trait.Borrow.html\" title=\"trait core::borrow::Borrow\">Borrow</a>&lt;T&gt; for <a class=\"struct\" href=\"cactusref/struct.Rc.html\" title=\"struct cactusref::Rc\">Rc</a>&lt;T&gt;",synthetic:false,types:["cactusref::rc::Rc"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/ops/deref/trait.Deref.js
+++ implementors/core/ops/deref/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["cactusref"] = [{text:"impl&lt;T:&nbsp;?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> + <a class=\"trait\" href=\"cactusref/trait.Reachable.html\" title=\"trait cactusref::Reachable\">Reachable</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"cactusref/struct.Rc.html\" title=\"struct cactusref::Rc\">Rc</a>&lt;T&gt;",synthetic:false,types:["cactusref::rc::Rc"]},];
+implementors["cactusref"] = [{text:"impl&lt;T:&nbsp;?<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"cactusref/struct.Rc.html\" title=\"struct cactusref::Rc\">Rc</a>&lt;T&gt;",synthetic:false,types:["cactusref::rc::Rc"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
