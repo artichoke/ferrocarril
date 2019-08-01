8d86aefb7f745ca016c9e115f7c6318639120210
ci-build <ci-build@hyperbo.la>
2019-08-01 04:08:10 +0000

    [skip ci] generate docs

--- implementors/core/convert/trait.From.js
+++ implementors/core/convert/trait.From.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
 implementors["mruby"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"mruby_sys/ffi/struct.mrb_value.html\" title=\"struct mruby_sys::ffi::mrb_value\">mrb_value</a>&gt; for <a class=\"enum\" href=\"mruby/value/types/enum.Ruby.html\" title=\"enum mruby::value::types::Ruby\">Ruby</a>",synthetic:false,types:["mruby::value::types::Ruby"]},];
-implementors["nemesis"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;MrbError&gt; for <a class=\"enum\" href=\"nemesis/enum.Error.html\" title=\"enum nemesis::Error\">Error</a>",synthetic:false,types:["nemesis::Error"]},];
+implementors["nemesis"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"mruby/enum.MrbError.html\" title=\"enum mruby::MrbError\">MrbError</a>&gt; for <a class=\"enum\" href=\"nemesis/enum.Error.html\" title=\"enum nemesis::Error\">Error</a>",synthetic:false,types:["nemesis::Error"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
